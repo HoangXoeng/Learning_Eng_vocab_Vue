14b3013c342241c34e91068dbe608ac0732f69ee
Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2024-09-25 16:21:31 +0700

    done project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
+
   <div id="app">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div class="container">
-      <!-- Header begin -->
       <AppHeader />
-      <!-- Header end -->
-      <!-- Body begin -->
       <div class="body">
         <div class="body__content col-12">
-          <div class="body__content__add col-4" @click="showAddModal">
+          <!-- Nút bấm mở form thêm từ vựng -->
+          <div class="body__content__add col-4" @click="showForm">
             <div class="body__content__add--top">
               <div class="body__content__add__icon">
                 <i class="fa-solid fa-plus body__content__add__icon__plus"></i>
@@ -19,55 +17,222 @@
               <span class="body__content__add__title roboto-bold">Create new one</span>
             </div>
           </div>
+          <!-- End -->
+          <!-- Dữ liệu các từ vuwnjgsex được trình bày ở đây -->
+          <div class="body__content__item col-4" v-for="vocab in vocabList" :key="vocab.id" :id="vocab.id"
+            @click="showDetail(vocab.id,vocab.img,vocab.word,vocab.meaning,vocab.example)">
+            <div class="body__content__item--top">
+              <img :src="vocab.img" alt="" class="body__content__item__img" />
+              <span class="body__content__item__word roboto-medium">{{ vocab.word }}</span>
+            </div>
+            <div class="body__content__item--bot">
+              <span class="body__content__item__meaning roboto-bold">{{ vocab.meaning }}</span>
+            </div>
+          </div>
+          <!-- End -->
         </div>
       </div>
     </div>
 
-    <div class="add__form" v-if="isAddModalVisible" @click="showAddModal">
-      <form action="" name="frm" id="formAdd">
-        <i class="fa-solid fa-x" id="close__form" @click="hideFormAndModal"></i>
+    <!-- Modal form để thêm từ vựng -->
+    <div class="add__form" v-if="isAddModalVisible">
+      <form  name="frm" id="formAdd">
+
+        <div id="close--form" @click="hideForm">
+          <i class="fa-solid fa-x" id="close__form"></i>
+        </div>
         <label for="word">Word</label>
-        <input type="text" id="word" name="word" required /> <br /><br />
+        <input type="text" id="word" name="word" v-model="word" required />
+        <br /><br />
+
         <label for="meaning">Meaning</label>
-        <input type="text" id="meaning" name="meaning" required /> <br /><br />
+        <input type="text" id="meaning" name="meaning" v-model="meaning" required />
+        <br /><br />
+
         <label for="example">Example</label>
-        <textarea id="example" name="example" required></textarea>
+        <textarea id="example" name="example" v-model="example" required></textarea>
         <br /><br />
-        <input type="file" id="img_link" name="img_link" accept="image/*" required />
+
+        <input type="file" id="img_link" name="img_link" accept="image/*" @change="handleFileUpload" required />
         <br /><br />
-        <button type="button" onclick="addNewVocab()">Create</button>
+
+        <button type="button" @click="addNewVocab" >Create</button>
+
       </form>
     </div>
-    <div id="detail__form__contain">
-    </div>
-    <div class="modal" v-if="isAddModalVisible" @click="hideFormAndModal"></div>
+    <!-- Overlay modal -->
+    <div class="modal" v-if="isAddModalVisible" @click="hideForm"></div>
+    <div class="modal" v-if="isDetailFormVisible" @click="hideDetailForm"></div>
+    <!-- Form chi tieetr vocabulary -->
+  
+      <div class="detail__form" v-if="isDetailFormVisible">
+        <div class="detail__form__content">
+          <div class="detail__form__content__top">
+            <div class="detail__form__content__top--icon" @click="hideDetailForm()">
+              <i class="fa-solid fa-x"></i>
+            </div>
+             <div class="detail__form__content__top__delete--icon" @click="deleteItem(idInDetail)">
+              <i class="fa-solid fa-trash"></i>
+            </div>
+
+           <div class="detail__form__content__img">
+            <img :src="imgInDetail" alt="" id="detail__form__content__img--img">
+           </div>
+          </div>
+          <div class="detail__form__content__bot">
+            <div class="detail__form__content__bot--item">
+              <span class="detail__form__content__bot--word roboto-bold">Word:</span>
+              <span class="detail__form__content__bot--word-value">{{ wordInDetail }}</span>
+            </div>
+            <div class="detail__form__content__bot--item">
+              <span class="detail__form__content__bot--meaning roboto-regular">Meaning:</span>
+              <span class="detail__form__content__bot--meaning-value">{{ meaningInDetail }}</span>
+            </div>
+            <div class="detail__form__content__bot--item">
+              <span class="detail__form__content__bot--example roboto-regular">Example:</span>
+              <span class="detail__form__content__bot--example-value">{{ exampleInDetail }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- end -->
   </div>
 </template>
-
-
-
-
+<!-- Vieets script -->
 <script setup>
-// const word = ref("");
-import { ref } from 'vue'
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
 import AppHeader from './components/AppHeader.vue'
+// Khai báo các biến reactive/ref
+const word = ref("");
+const meaning = ref("");
+const example = ref("");
+const imgLink = ref(null);
+var isAddModalVisible = ref(false);
+const vocabList = ref([]);
+var isDetailFormVisible = ref(false);
 
-const isAddModalVisible = ref(false);
-const hideFormAndModal = () => {
- isAddModalVisible.value = false;
+const idInDetail = ref("");
+const imgInDetail = ref("");
+const wordInDetail = ref("");
+const meaningInDetail = ref("");
+const exampleInDetail = ref("");
+
+
+
+const showDetail = (id,img,word,meaning,example) => {
+  isDetailFormVisible.value = true;
+  wordInDetail.value = word;
+  idInDetail.value = id;
+  imgInDetail.value = img;
+  meaningInDetail.value = meaning;
+  exampleInDetail.value = example;
 }
-
-const showAddModal = () => {
+const hideDetailForm = () =>{
+  isDetailFormVisible.value = false;
+}
+var refreshPage = () => {
+      window.location.reload();
+    }
+// Hiển thị form thêm từ vựng
+var showForm = () => {
   isAddModalVisible.value = true;
-}
+};
+// Ẩn form thêm từ vựng
+var hideForm = () => {
+  isAddModalVisible.value = false;
+};
+// Xử lý khi upload hình ảnh
+const handleFileUpload = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      imgLink.value = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
+// Gửi dữ liệu từ vựng lên server
+const addNewVocab = () => {
+  var opt = {
+    url: "http://localhost:3000/vocabularys",
+    method: "post",
+    data: {
+      word: word.value,
+      meaning: meaning.value,
+      example: example.value,
+      img: imgLink.value,
+    },
+  };
+  axios(opt)
+    .then(function (data_res) {
+      console.log(data_res);
+      if (data_res.status == 201) console.log("Add data successfully");
+    })
+    .catch(function (ex) {
+      console.log(ex);
+    });
+    // ẩn forrm
+    isAddModalVisible.value = false;
+    // refresh lại trang web
+   
+    refreshPage()
+};
+// Hàm lấy dữ liệu từ vựng từ server bằng fetch
+// const vocabApi = "http://localhost:3000/vocabularys";
+// const getVocabulary = async () => {
+//   try {
+//     const response = await fetch(vocabApi);
+//     const vocabs = await response.json();
+//     vocabList.value = vocabs;
+//   } catch (error) {
+//     console.error("Lỗi khi lấy dữ liệu từ vựng:", error);
+//   }
+// };
+
+  const deleteItem = (id) => {
+    console.log("Deleting"+id)
+    axios
+    .delete(`http://localhost:3000/vocabularys/${id}`)
+    .then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        // Xóa phần tử khỏi DOM
+        // document.getElementById(vocabId).remove();
+        alert("Delete item complete");
+      }
+    })
+    .catch((error) => {
+      console.error("Error deleting item:", error);
+      alert("An error occurred while deleting the item.");
+    });
+    refreshPage();
+  }
+
+
+
+
+
+
+  onMounted( async () => {
+     try {
+        const response = await axios.get("http://localhost:3000/vocabularys")
+        vocabList.value = response.data;
+     } catch (error) {
+      console.log("Error:", error);
+     }
+  });
 
 </script>
-
+<!-- CSS viết ở đây -->
 <style>
 /* Body */
 .body {
   margin-top: 88px;
 }
+
 
 .body__content {
   display: grid;
@@ -238,13 +403,13 @@ form[name="frm"] button:hover {
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   padding: 20px;
-  display: none;
+  /* display: none; */
   justify-content: center;
   align-items: center;
-  position: absolute;
+  position: fixed;
   left: 50%;
   top: 50%;
-  transform: translateX(-50%);
+  transform: translate(-50%,-50%);
   z-index: 2;
 
 }
